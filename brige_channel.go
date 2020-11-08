cdd5dd821f8eb8c6c5d3a78da8442a640ea5c9ec
JimmyLee <ljming1106@163.com>
2020-11-09 00:28:17 +0800

    Create brige_channel.go

---
+++ brige_channel.go
@@ -0,0 +1,78 @@
+/*
+  －系列 的 channel 需要有序地写入及结果输出 ，即使是不同的来源
+*/
+package main
+
+import "fmt"
+
+func main() {
+	for v := range brige(nil, genVals()) {
+		fmt.Printf("%v  ", v)
+	}
+}
+
+func genVals() <-chan <-chan interface{} {
+	chanStream := make(chan (<-chan interface{}))
+	go func() {
+		defer close(chanStream)
+		for i := 0; i < 10; i++ {
+			stream := make(chan interface{}, 1)
+			stream <- i
+			close(stream)
+			chanStream <- stream
+		}
+	}()
+	return chanStream
+}
+
+func brige(
+	done <-chan interface{},
+	chanStream <-chan <-chan interface{},
+) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		// 顺序从channel chanStream中读取chanStream中各个channel的值
+		for {
+			var stream <-chan interface{}
+			select {
+			case maybeStream, ok := <-chanStream:
+				if ok == false {
+					return
+				}
+				stream = maybeStream
+			case <-done:
+				return
+
+			}
+			for val := range orDone(done, stream) {
+				select {
+				case valStream <- val:
+				case <-done:
+				}
+			}
+		}
+	}()
+	return valStream
+}
+
+func orDone(done, c <-chan interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			select {
+			case <-done:
+			case v, ok := <-c:
+				if ok == false {
+					return
+				}
+				select {
+				case valStream <- v:
+				case <-done:
+				}
+			}
+		}
+	}()
+	return valStream
+}
